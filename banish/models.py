3e739706dfbb0c9095707712235ca3d84bdbb0c5
davidchern <verse@live.com>
2020-01-14 06:15:47 +0800

    Update models.py
    Simplify the definition of models, and make it compatible to Django 2.2.x.

--- banish/models.py
+++ banish/models.py
@@ -19,8 +19,8 @@ from django.db import models
 from django.db.models.signals import post_save, post_delete
 from django.core.cache import cache
 
-ABUSE_PREFIX = 'DJANGO_BANISH_ABUSE:'
 BANISH_PREFIX = 'DJANGO_BANISH:'
+ABUSE_PREFIX = 'DJANGO_BANISH_ABUSE:'
 WHITELIST_PREFIX = 'DJANGO_BANISH_WHITELIST:'
 
 
@@ -29,32 +29,32 @@ class Banishment(models.Model):
     # Flush out time constrained banned in future revisions
     # ban_start = models.DateField(help_text="Banish Start Date.")
     # ban_stop = models.DateField(help_text="Banish Stop Date.")
-    # ban_is_permanent = models.BooleanField(help_text="Is Ban Permanent? (Start/Stop ignored)")
-
-    ban_reason = models.CharField(max_length=255, help_text="Reason for the ban?")
+    # ban_is_permanent = models.BooleanField(help_text="Is Ban Permanent?")
 
     BAN_TYPES = (
-        ('ip-address', 'IP Address'),
-        ('user-agent', 'User Agent'),
+        ('ip-address', 'Banned IP Address'),
+        ('user-agent', 'Banned User Agent'),
+        ('ip-address-whitelist', 'Whitelist IP Address'),
     )
 
-    type = models.CharField(
+    kind = models.CharField(
         max_length=20,
         choices=BAN_TYPES,
         default=0,
-        help_text="Type of User Ban to store"
+        db_index=True,
+        help_text="Type of User Ban/Whitelist to store"
     )
 
     condition = models.CharField(
         max_length=255,
-        help_text='Enter an IP to ban/whitelist, or a User Agent string to ban'
+        db_index=True,
+        help_text='Enter an IP to ban/whitelist, or a User Agent to ban'
     )
 
-    def __unicode__(self):
-        return "Banished %s %s " % (self.type, self.condition)
+    count = models.PositiveSmallIntegerField(default=1, help_text='Banned count')
 
     def __str__(self):
-        return self.__unicode__()
+        return "%s %s" % (self.get_kind_display(), self.condition)
 
     def is_current(self):
         """
@@ -66,68 +66,34 @@ class Banishment(models.Model):
         return False
 
     class Meta:
-        permissions = (("can_ban_user", "Can Ban User"),)
-        verbose_name = "Banishment"
-        verbose_name_plural = "Banishments"
+        #permissions = (("can_manage_access", "Can Manage Access"),)
+        verbose_name = "User Access"
+        verbose_name_plural = "User Access"
         db_table = 'banishments'
 
 
-class Whitelist(models.Model):
-    whitelist_reason = models.CharField(max_length=255, help_text="Reason for the whitelist?")
-
-    WHITELIST_TYPES = (
-        ('ip-address-whitelist', 'Whitelist IP Address'),
-    )
-
-    type = models.CharField(
-        max_length=20,
-        choices=WHITELIST_TYPES,
-        default=0,
-        help_text='Enter an IP address to whitelist'
-    )
-
-    condition = models.CharField(
-        max_length=255,
-        help_text='Enter an IP to whitelist'
-    )
-
-    def __unicode__(self):
-        return "Whitelisted %s %s " % (self.type, self.condition)
-
-    def __str__(self):
-        return self.__unicode__()
-
-    class Meta:
-        permissions = (("can_whitelist_user", "Can Whitelist User"),)
-        verbose_name = "Whitelist"
-        verbose_name_plural = "Whitelists"
-        db_table = 'whitelists'
-
-
 def _generate_cache_key(instance):
-    if instance.type == 'ip-address-whitelist':
-         cache_key = WHITELIST_PREFIX + instance.condition
-    if instance.type == 'ip-address':
-         cache_key = BANISH_PREFIX + instance.condition
+    if instance.kind == 'ip-address-whitelist':
+        cache_key = WHITELIST_PREFIX + instance.condition
+        count = 12
+    if instance.kind == 'ip-address':
+        cache_key = BANISH_PREFIX + instance.condition
+        count = instance.count
     abuse_key = ABUSE_PREFIX + instance.condition
-    return cache_key, abuse_key
+    return (cache_key, abuse_key, count)
 
 
-def _update_cache(sender, **kwargs):
+def _update_cache(sender, instance, **kwargs):
     # add a whitelist entry and remove any abuse counter for an IP
-    instance = kwargs.get('instance')
-    cache_key, abuse_key = _generate_cache_key(instance)
-    cache.set(cache_key, "1")
+    cache_key, abuse_key, count = _generate_cache_key(instance)
+    cache.set(cache_key, 1, 3600 * count)
     cache.delete(abuse_key)
 
 
-def _delete_cache(sender, **kwargs):
-    instance = kwargs.get('instance')
-    cache_key, abuse_key = _generate_cache_key(instance)
+def _delete_cache(sender, instance, **kwargs):
+    cache_key, abuse_key, _ = _generate_cache_key(instance)
     cache.delete(cache_key)
 
 
-post_save.connect(_update_cache, sender=Whitelist)
 post_save.connect(_update_cache, sender=Banishment)
-post_delete.connect(_delete_cache, sender=Whitelist)
 post_delete.connect(_delete_cache, sender=Banishment)
